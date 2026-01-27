377052d97adac812c9e4c555dcc98e57e26103e3
Avishkar05 <avishkarzende505@gmail.com>
2026-01-27 17:08:13 +0530

    update Mountphase

--- src/Mountphase.jsx
+++ src/Mountphase.jsx
@@ -9,6 +9,20 @@ export default class Mountphase extends Component {
     update=()=>{
         this.setState({"counter":this.state.counter+1})
     }
+
+    componentDidMount(){
+        console.log("this is example of componentDidMount ")
+        document.addEventListener("keydown", this.handleKeyPress)
+    }
+
+    // componentWillUnmount() {
+    //     document.removeEventListener("keydown", this.handleKeyPress)
+    // }
+
+    handleKeyPress=(event)=>{
+        console.log(event.key)
+    }
+
     render() {
       console.log("This is  Render  in Class");
 
@@ -17,6 +31,8 @@ export default class Mountphase extends Component {
         <h1>counter is {this.state.counter}</h1>
         <button onClick={this.update}>update counter</button>
         
+        <h1>--------------Event Handling----------------</h1>
+        Enter name: <input type="text" />
         
       </div>
     )
